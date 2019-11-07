30c096cf3c5776668b3cfa12d869863b609c9e36
Bordin Hongprapat <bordin.hongprapat@gmail.com>
2019-11-07 13:35:49 +0700

    change HandleAjaxMedia namespace

--- src/Traits/HandleAjaxMedia.php
+++ src/Traits/HandleAjaxMedia.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Gaspertrix\Backpack\DropzoneField\Traits;
+namespace Bearth\Backpack\DropzoneField\Traits;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
