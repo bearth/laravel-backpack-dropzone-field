af35f938fcb1073d15a317eebd76688480727866
Bordin Hongprapat <bordin.hongprapat@gmail.com>
2019-11-07 15:23:04 +0700

    install namespace

--- src/app/Console/Commands/Install.php
+++ src/app/Console/Commands/Install.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Gaspertrix\Backpack\DropzoneField\App\Console\Commands;
+namespace Bearth\Backpack\DropzoneField\App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Symfony\Component\Process\Process;
